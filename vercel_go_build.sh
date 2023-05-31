7203bc02a46b3979410f3fe5d3a5ef8ec6a3d2fd
nekiwo <58634064+nekiwo@users.noreply.github.com>
2023-05-30 19:17:18 -0500

    go build fix

--- vercel_go_build.sh
+++ vercel_go_build.sh
@@ -4,3 +4,4 @@ wget https://go.dev/dl/go1.20.4.linux-amd64.tar.gz
 rm -rf /usr/local/go && tar -C /usr/local -xzf go1.20.4.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
 go version
+go build ./cmd/imsa-grades
