08f5e7e9367e9511f35bedb68b2138b686217204
nekiwo <nekiwo@users.noreply.github.com>
2023-08-06 09:37:10 -0500

    fix?

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ let header, classes = [];
 let labels = ['4.0', '3.67', '3.33', '3.0', '2.67', '2.33', '2.0', '1.67', '1.0'];
 let labelText = ['A (4.0)', 'A- (3.67)', 'B+ (3.33)', 'B (3.0)', 'B- (2.67)', 'C+ (2.33)', 'C (2.0)', 'C- (1.67)', 'D (1.0)'];
 
-const headboilerplate = fs.readFileSync("./components/header.html", {encoding: "utf8", flag: "r"})
+const headboilerplate = fs.readFileSync(path.join(__dirname, "/components/header.html"), {encoding: "utf8", flag: "r"})
 
-const PORT = 3000 || process.env.PORT
+const PORT = 5000 || process.env.PORT
 
 app.use(express.static(path.join(__dirname + '/public')));
 
